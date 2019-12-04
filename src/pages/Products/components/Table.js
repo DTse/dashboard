71456a64cc008bd229c33c546dedcf16cf614572
Dimitris Tsetsonis <dimitristsetsonis@Dimitriss-iMac.star.gr>
2019-12-04 18:34:19 +0200

    fix: Table replace Fragment with div

--- src/pages/Products/components/Table.js
+++ src/pages/Products/components/Table.js
@@ -1,11 +1,12 @@
 import React, {memo} from 'react';
 
 import TableItem from './TableItem';
+
 const Table = memo(({products}) =>{
     return (
-        <>
+        <div>
             {products && products.map((product, key) => <TableItem key={'product-'+key+'-'+Math.random()} product={product}/>)}
-        </>
+        </div>
     )
 });
 
