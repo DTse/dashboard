4c585e17f0ce778b59089deae58a0405e054e69d
Dimitris Tsetsonis <dimitristsetsonis@Dimitriss-iMac.star.gr>
2019-12-06 12:31:02 +0200

    style: remove unused index.css, prettify index.js, optimize images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { AppContextProvider } from './store/AppContext';
 
-ReactDOM.render(
-    <AppContextProvider><App /></AppContextProvider>
-, document.getElementById('root'));
+render(
+    <AppContextProvider>
+        <App />
+    </AppContextProvider>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
