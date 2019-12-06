70a4e3f437728e9fb814a42b53a1320a170a12d5
Dimitris Tsetsonis <dimitristsetsonis@Dimitriss-iMac.star.gr>
2019-12-06 12:41:36 +0200

    refactor: refactor product item

--- src/pages/Products/components/TableItem.js
+++ src/pages/Products/components/TableItem.js
@@ -1,5 +1,10 @@
 import React, {memo} from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import ProductImage from '../../../components/ProductImage';
+import UserAvatar from '../../../components/UserAvatar';
+import Votes from '../../../components/Votes';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -7,79 +12,85 @@ const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex', 
         flexDirection: 'row',
-        alignItems: 'center',
-        padding: '20px 0px'
+        alignItems: 'flex-start',
+        width: '100%',
+        maxWidth: 600,
+        padding: '10px 0px',
+        [theme.breakpoints.down('xs')]:{
+            flexDirection: 'column'
+        }
     },
     title:{
-        margin: '10px 0 0 0',
+        margin: '8px 0 0 0',
+        lineHeight: '20px',
         '&:hover':{
             textDecoration: 'underline'
         },
         '& > a': {
-            fontSize: 16,
+            fontSize: 14,
             textDecoration: 'none',
-            color: '#2196f3'
-        }
-    },
-    productImg: {
-        display: 'flex',
-        position: 'relative',
-        maxWidth: 115,
-        overflow: 'hidden',
-        flex: '1 0',
-        marginRight: 20,
-        '&::before':{
-            content: '""',
-            display: 'block',
-            paddingTop: '82.91%'
-        },
-        '& > img':{
-            position: 'absolute',
-            top:0,
-            width: '100%',
-            minHeight: 70,
-            display: 'block'
+            color: '#3483cb'
         }
     },
     author:{
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        marginTop: 10
-    },
-    authorImg:{
-        height: 25,
-        width: 25,
-        display: 'inline-block',
-        borderRadius: '50%'
+        marginTop: 8
     },
     authorDescr:{
         marginRight: 5,
         color: '#757575',
         fontSize: 13,
         marginTop: 5
-    },
-    votes:{
-        fontSize: 20,
-        fontWeight: 700
     }
 }));
-
-const TableItem = memo(({product})=>{
+/**
+   * Return the user image located in the assets folder.
+   * @param {object} product
+   * @return {any}
+**/ 
+const TableItem = ({product})=>{
     const classes = useStyles();
 
+    const {
+        id,
+        title,
+        description,
+        url,
+        votes,
+        submitterAvatarUrl,
+        productImageUrl
+    } = product;
 
     return(
         <div className={classes.root}>
-            <div className={classes.productImg}><img alt={`Product ${product.id}`} src={require('../../../'+product.productImageUrl)}/></div>
+            <ProductImage 
+                alt={`Product ${id}`}
+                src={productImageUrl}
+            />
             <div>
-                <div className={classes.votes}>{product.votes}</div>
-                <h2 className={classes.title}><Link to={`/${product.url}`}>{product.title}</Link></h2>
-                <div>{product.description}</div>
-                <div className={classes.author}><span className={classes.authorDescr}>Submitted by: </span><img className={classes.authorImg} alt={`User of product ${product.id}`} src={require('../../../'+product.submitterAvatarUrl)} /></div>
+                <Votes id={id} votes={votes} />
+                <h2 className={classes.title}>
+                    <Link to={`/${url}`}>
+                        {title}
+                    </Link>
+                </h2>
+                <div>{description}</div>
+                <div className={classes.author}>
+                    <span className={classes.authorDescr}>Submitted by: </span>
+                    <UserAvatar 
+                        alt={`User of product ${id}`} 
+                        src={submitterAvatarUrl} 
+                    />
+                </div>
             </div>
         </div>
     )
-})
+}
+
+TableItem.propTypes = {
+    product: PropTypes.object.isRequired,
+};
 
 export default TableItem
